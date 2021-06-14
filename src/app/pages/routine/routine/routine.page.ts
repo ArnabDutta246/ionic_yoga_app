4c8844ee6c5e533c506e80a20d2c761a006ba303
Arnab Dutta <arnab2461997@gmail.com>
2021-06-14 08:29:35 +0600

    custom calender

--- src/app/pages/routine/routine/routine.page.ts
+++ src/app/pages/routine/routine/routine.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-routine',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./routine.page.scss'],
 })
 export class RoutinePage implements OnInit {
-
-  constructor() { }
+  currWeekList: any = [];
+  currDayIndex: number = 0;
+  constructor() {}
 
   ngOnInit() {
+    this.setWeek();
+  }
+  public setWeek(): void {
+    let sunday: string = moment().day(0).format();
+    this.currWeekList.push(sunday);
+    for (let i = 1; i < 7; i++) {
+      let day = moment().day(i).format();
+      this.currWeekList.push(day);
+      if (moment().format() == day) this.currDayIndex = i;
+    }
+    console.log(this.currWeekList);
   }
 
+  public fetchRoutine(i: number): void {
+    this.currDayIndex = i;
+  }
 }
